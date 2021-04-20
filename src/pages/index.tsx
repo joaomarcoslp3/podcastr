184c5bd2039d266b097d212abb6a61c19d2181e3
João Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2021-04-20 10:54:47 -0300

    static site request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import { Header } from "../components";
 
-export default function Home() {
+export default function Home (props) {
   return (
     <h1>Alo</h1>
+    
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
