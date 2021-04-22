8363f8b07578905816c46895497dc37705e1c5de
João Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2021-04-22 17:16:48 -0300

    created play audio function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,39 @@
+import { useState } from 'react'
+
 import { Header, Player } from '../components'
+import { PlayerContext } from '../providers'
 import styles from '../styles/app.module.scss'
 import '../styles/global.scss'
+
 function MyApp({ Component, pageProps }) {
+  const [episodesList, setEpisodesList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  function play (episode) {
+    setEpisodesList([episode])
+    setCurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  function togglePlay () {
+    setIsPlaying(!isPlaying)
+  }
+
+  function setPlayingState (state: boolean) {
+    setIsPlaying(state)
+  }
+
   return (
-     <div className={styles.wrapper}>
-       <main>
-        <Header />
-        <Component {...pageProps} />
-       </main>
-       <Player />
-     </div>
+    <PlayerContext.Provider value={{episodesList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState}}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+     </PlayerContext.Provider>
   )
 }
 
